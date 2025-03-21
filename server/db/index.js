987fe908f3896e86410b7ee3e0aa70881b031179
Mohamed-Messaoudene <127661213+Mohamed-Messaoudene@users.noreply.github.com>
2025-03-21 20:12:17 +0100

    resolving db connection

--- server/db/index.js
+++ server/db/index.js
@@ -2,12 +2,13 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 // Create a Sequelize instance (connection)
+const host_name = process.env.HOST_NAME;
 const db_name = process.env.DATABASE_NAME;
 const username = process.env.USER_NAME;
 const password = process.env.PASSWORD;
 const sequelize = new Sequelize(db_name, username, password, {
-  host: "localhost", // Your PostgreSQL host
-  dialect: "postgres", // The database you're using
+  host: host_name, 
+  dialect: "postgres",
 });
 // Test the connection
 sequelize
